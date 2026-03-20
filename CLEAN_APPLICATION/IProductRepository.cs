0b56013b33bcd7474236ba454cc7d37b64681cd6
maaitlunghau <chunhau.py@gmail.com>
2026-03-20 20:25:28 +0700

    feat(application): add repository and service of product

---
+++ CLEAN_APPLICATION/IProductRepository.cs
@@ -0,0 +1,12 @@
+using CLEAN_DOMAIN;
+
+namespace CLEAN_APPLICATION;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetProductsAsync();
+    Task<Product> GetProductAsync(Guid id);
+    Task<Product> AddAsync(Product pro);
+    Task<Product> UpdateAsync(Product pro);
+    Task DeleteAsync(Guid id);
+}
---
+++ CLEAN_APPLICATION/ProductService.cs
@@ -0,0 +1,35 @@
+using CLEAN_DOMAIN;
+
+namespace CLEAN_APPLICATION;
+
+public class ProductService
+{
+    private readonly IProductRepository _productRepository;
+    public ProductService(IProductRepository productRepository)
+        => _productRepository = productRepository;
+
+    public async Task<IEnumerable<Product>> GetAllProducts()
+    {
+        return await _productRepository.GetProductsAsync();
+    }
+
+    public async Task<Product> GetOneProduct(Guid id)
+    {
+        return await _productRepository.GetProductAsync(id);
+    }
+
+    public async Task AddProduct(Product pro)
+    {
+        await _productRepository.AddAsync(pro);
+    }
+
+    public async Task UpdateProduct(Product pro)
+    {
+        await _productRepository.UpdateAsync(pro);
+    }
+
+    public async Task DeleteProduct(Guid id)
+    {
+        await _productRepository.DeleteAsync(id);
+    }
+}
