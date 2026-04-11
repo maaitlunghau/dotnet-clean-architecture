5bd26795386d14403ed7232daf282f77d2f6b188
maaitlunghau <chunhau.py@gmail.com>
2026-04-11 20:56:43 +0700

    feat: configure Swagger UI for testing API and auto-redirect to '/swagger'

--- CLEAN.API/Program.cs
+++ CLEAN.API/Program.cs
@@ -1,10 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    //
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.MapControllers();
+
+app.MapGet("/", () => Results.Redirect("/swagger/index.html"));
+
 app.UseHttpsRedirection();
+
+app.Run();
