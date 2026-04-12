cfc6579ed12930ca80aa5debd0a87d66311e3adf
maaitlunghau <chunhau.py@gmail.com>
2026-04-12 20:44:17 +0700

    feat(api): configure connection string MySQL and register DI

--- CLEAN.API/Program.cs
+++ CLEAN.API/Program.cs
@@ -1,8 +1,26 @@
+using CLEAN.APPLICATION.Interfaces;
+using CLEAN.APPLICATION.Services;
+using CLEAN.DOMAIN.Entities;
+using CLEAN.DOMAIN.Interfaces;
+using CLEAN.INFRASTRUCTURE.Persistence;
+using CLEAN.INFRASTRUCTURE.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("MySQL");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString))
+    );
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
@@ -13,6 +31,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 app.MapGet("/", () => Results.Redirect("/swagger/index.html"));
 
